26a062dda9eeed87e084d7188ae411aa94a4bf89
Xelynega <Xelynega@users.noreply.github.com>
2017-09-21 18:38:11 -0400

    Added new features
    Added ability for new fields to be added in KiCad without any need for refactoring the script. Added warning message for parts that are in the netlist, but missing from the component library.

--- bom/generate_warg_bom.py
+++ bom/generate_warg_bom.py
@@ -6,6 +6,7 @@ except ImportError:
     print "lxml, a python library for handling xml files, is not downloaded"
     pip.main(['install', 'lxml'])
     from lxml import etree as ET
+
 import sys, os
 import csv
 
@@ -18,48 +19,86 @@ def main():
     #output_dict is the dictionary that gets written to the csv file
     found_parts = []
     output_dict = []
+    keylist = ['ref', 'quantity']
 
     #Parse the netlist and store it in an lxml tree
     tree = ET.parse(netlist_xml)
+
+    #Get a list of all the keys for the CSV at the end
+    for sibling in tree.findall(".//*[@name='part_num']"):
+        if sibling.getparent().getparent().tag == 'comp':
+            parent = sibling.getparent()
+            for child in parent:
+                if child.attrib['name'] not in keylist:
+                    keylist.append(child.attrib['name'])
+
 
     #Traverse over every element with a WARG Part Number 
     for field in tree.findall(".//*[@name='part_num']"):
         #Check if the element is under a component or a library element
         if field.getparent().getparent().tag == 'comp':
-            no_load = False
-            #Check if the part is a no_load part
-            if(field.getparent().find(".//*[@name='no_load']") > 0):
-                new_text = field.text + "-NOLD"
-                no_load = True
-            else:
-                new_text = field.text
+            part = dict()
+            for child in field.getparent():
+                part.update({child.attrib['name']:child.text})
 
             ref = field.getparent().getparent().attrib['ref']
             
-            #Add the part to the dictionaries that it needs to be in
-            if new_text not in found_parts:
-                found_parts.append(new_text)
-                output_dict.append({'part_num':field.text, 'quantity':1, 'ref':ref, 'no_load':no_load})
-            else:
-                output_dict[found_parts.index(new_text)]['quantity'] += 1
-                output_dict[found_parts.index(new_text)]['ref'] += (', ' + ref)
+            #Find a matching part to add a quantity
+            matching_part = {}
+            for check in found_parts:
+                #Check if they both have the same keys
+                if check.keys() == part.keys():
+                    #Check if all the values are the same
+                    for key in check.keys():
+                        if check[key] != part[key]:
+                            break
+                        matching_part = check
 
+            if matching_part == {}:      
+                found_parts.append(part.copy())
+                part.update({'quantity':1, 'ref':ref})
+                output_dict.append(part)
+            else:
+                output_dict[found_parts.index(part)]['quantity'] += 1
+                output_dict[found_parts.index(part)]['ref'] += (', ' + ref)
     reader = csv.DictReader(open('component_library.csv', 'rb'))
 
+    #Add the component library keys to the keylist
+    for key in reader.fieldnames:
+        if key not in keylist:
+            keylist.append(key)
+
     #Add the values from the component_library to the output dictionary
-    component_library_dict = []
     for line in reader:
         if "part_num" not in line:
             print "line doesn't have part_num! line: ", line
-        elif line['part_num'] in found_parts:
-            output_dict[found_parts.index(line['part_num'])].update(line)
+        for part in output_dict:
+            if part['part_num'] == line['part_num']:
+                part.update(line)
+
+    #Check for parts missing from the library
+    missing_parts = []
+    for part in output_dict:
+        try:   
+            if part['Distributer #'] != '':
+                continue
+        except:
+            missing_parts.append(part)
+
+    #Give error with missing parts
+    warning_message = "\n\tWARNING: Parts "
+    for p in missing_parts: 
+        warning_message += (p['part_num'] + ", ")
+    warning_message = warning_message[:-2] + " do not have matching distributor numbers in the component library!\n"
+
+    print warning_message
 
     #Write the output dictionary to disk as output_filename
     with open(output_filename, 'wb') as output_file:
-        dict_writer = csv.DictWriter(output_file, output_dict[0].keys())
+        dict_writer = csv.DictWriter(output_file, keylist)
         dict_writer.writeheader()
         dict_writer.writerows(output_dict)
-    print "wrote to {}".format(output_filename)
+    print "\tWrote to {}".format(output_filename)
 
 if __name__ == "__main__":
     main()
