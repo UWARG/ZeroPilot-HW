da041d12e51c46bd187a1fe5c2910741665e0c6b
Stephen Cholvat <Scholvat@uwaterloo.ca>
2018-06-05 11:59:18 -0400

    Added error checking

--- bom/generate_warg_bom.py
+++ bom/generate_warg_bom.py
@@ -17,7 +17,7 @@ def main():
     found_parts = []
     output_dict = []
     keylist = ['ref', 'quantity']
-
+    
     #Parse the netlist and store it in an lxml tree
     tree = ET.parse(netlist_xml)
 
@@ -29,36 +29,36 @@ def main():
                 if child.attrib['name'] not in keylist:
                     keylist.append(child.attrib['name'])
 
+    #Traverse over every component
+    for part_element in tree.findall(".//comp"):
+        part = dict()
+        fields = part_element.find('fields')
+        if(fields == None):
+            print "Warning: No part number for part",part_element.attrib['ref']
+            continue
+        for child in fields.findall('field'):
+            part.update({child.attrib['name']:child.text})
 
-    #Traverse over every element with a WARG Part Number 
-    for part_element in tree.findall(".//*[@name='part_num']...."):
-        #Check if the element is under a component or a library element
-        if part_element.tag == 'comp':
-            part = dict()
-            fields = part_element.find('fields')
-            for child in fields.findall('field'):
-                part.update({child.attrib['name']:child.text})
+        ref = part_element.attrib['ref']
+        
+        #Find a matching part to add a quantity
+        matching_part = {}
+        for check in found_parts:
+            #Check if they both have the same keys
+            if check.keys() == part.keys():
+                #Check if all the values are the same
+                for key in check.keys():
+                    if check[key] != part[key]:
+                        break
+                    matching_part = check
 
-            ref = part_element.attrib['ref']
-            
-            #Find a matching part to add a quantity
-            matching_part = {}
-            for check in found_parts:
-                #Check if they both have the same keys
-                if check.keys() == part.keys():
-                    #Check if all the values are the same
-                    for key in check.keys():
-                        if check[key] != part[key]:
-                            break
-                        matching_part = check
-
-            if matching_part == {}:      
-                found_parts.append(part.copy())
-                part.update({'quantity':1, 'ref':ref})
-                output_dict.append(part)
-            else:
-                output_dict[found_parts.index(part)]['quantity'] += 1
-                output_dict[found_parts.index(part)]['ref'] += (', ' + ref)
+        if matching_part == {}:
+            found_parts.append(part.copy())
+            part.update({'quantity':1, 'ref':ref})
+            output_dict.append(part)
+        else:
+            output_dict[found_parts.index(part)]['quantity'] += 1
+            output_dict[found_parts.index(part)]['ref'] += (', ' + ref)
     reader = csv.DictReader(open(component_lib_filename, 'rb'))
 
     #Add the component library keys to the keylist
@@ -70,17 +70,20 @@ def main():
     missing_parts = [0]*len(found_parts)
     for line in reader:
         if "part_num" not in line:
-            print "line doesn't have part_num! line: ", line
+            print "Error:line doesn't have part_num! line: ", line
         for part in output_dict:
             if part['part_num'] == line['part_num']:
                 part.update(line)
                 missing_parts[output_dict.index(part)] = 1
 
     print ''
+    error = False
     for index, part in enumerate(missing_parts):
         if part == 0:
-            print "\t{} is missing from the component library".format(output_dict[index]['part_num'])
-
+            error = True
+            print "Error:{} is missing from the component library".format(output_dict[index]['part_num'])
+    if(error):
+        sys.exit()
     #Write the output dictionary to disk as output_filename
     with open(output_filename, 'wb') as output_file:
         dict_writer = csv.DictWriter(output_file, keylist)
